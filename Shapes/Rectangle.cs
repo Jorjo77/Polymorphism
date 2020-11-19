5306e778502377fbc55344000efab47cf0e94cb2
Georgi Kolev <tuvkontrol55@gmail.com>
2020-11-19 16:47:04 +0200

    TasksUpdate

---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private readonly double width;
+        private readonly double hieght;
+
+        public Rectangle(double width, double hieght)
+        {
+            this.width = width;
+            this.hieght = hieght;
+        }
+
+        public override double CalculateArea()
+        {
+            return width * hieght;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * width + 2 * hieght;
+        }
+
+        public override string Draw()
+        {
+            return "Rectangle";
+        }
+    }
+}
---
+++ Shapes/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Shape rect = new Rectangle(3.0, 4.0);
+
+            Shape circle = new Circle(4.0);
+
+            Console.WriteLine(rect.CalculateArea());
+
+            Console.WriteLine(circle.CalculateArea());
+        }
+    }
+}
